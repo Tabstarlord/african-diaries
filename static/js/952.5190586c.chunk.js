8b47817c93dd746cce65102e95bc11de47aa2a46
Tabstarlord <henrystarizukanne@gmail.com>
2025-04-30 04:27:17 +0100

    Updates

---
+++ static/js/952.5190586c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkafrican_diaries=self.webpackChunkafrican_diaries||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.5190586c.chunk.js.map
